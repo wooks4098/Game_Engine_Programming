ecbe036f3044c95243ea8b01d06ceff64e9b83b4
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-03-31 16:27:30 +0900

    플레이어이동

---
+++ Assets/Wook/Click_Move.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Click_Move : MonoBehaviour
+{
+    public float Speed;//이동 속도
+
+
+    //필요한 컴포넌트
+    private Camera camera;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+
+        agent.updateRotation = false;//NavMeshAgent회전 제한
+        agent.speed = Speed;
+    }
+
+    private void Update()
+    {
+        ClickCheck();
+        Look_SetPoint();
+        EndMoveCheck();
+    }
+
+    //클릭했는지 확인하기
+    void ClickCheck()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                ClickMove(hit);
+
+            }
+        }
+    }
+
+    //클릭한 위치로 이동하기
+    void ClickMove(RaycastHit hit)
+    {
+        if (Vector3.Distance(transform.position, hit.point) > 0.2f)
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    //이동 위치 바라보기
+    void Look_SetPoint()
+    {
+        if (agent.hasPath)
+        {
+            //회전 미끄러짐 방지
+            agent.acceleration = (agent.remainingDistance < 2f) ? 8f : 60f;
+            //에이전트의 이동방향
+            Vector3 direction = agent.desiredVelocity;
+            //회전각도(쿼터니언)산출
+            Quaternion targetangle = Quaternion.LookRotation(direction);
+            //선형보간 함수를 이용해 부드러운 회전
+            animator.transform.rotation = Quaternion.Slerp(animator.transform.rotation, targetangle, Time.deltaTime * 8.0f);
+        }
+    }
+
+    //이동이 끝났는지 체크
+    void EndMoveCheck()
+    {
+        if (agent.velocity.sqrMagnitude >= 0.1f * 0.1f && agent.remainingDistance <= 0.1f)//이동종료
+        {
+            ResetMove();
+        }
+    }
+    //이동 초기화
+    void ResetMove()
+    {
+        agent.ResetPath();
+        agent.velocity = Vector3.zero;
+
+    }
+}
---
+++ Assets/Wook/Scripts/Item/ItemPiekUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPiekUp : MonoBehaviour
+{
+    [SerializeField]
+    private float PickUp_Range;     //습등 가능 거리
+    private bool PickUpActivate = false;        //습득 가능할 시 true;
+
+    private RaycastHit hitInfo;     //충동체 정보저장
+
+    //아이템 레이어를 담을 레이어 변수
+    [SerializeField]
+    private LayerMask LayerMask;
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private Text Item_Info_Text;
+    public GameObject Item_Info;
+
+    private void Update()
+    {
+        Check_Item();
+    }
+
+    //아이템 획득 시도
+    void Try_PickUp_Item()
+    {
+
+    }
+
+    //아이템 확인(마우스 커서를 올려 아이템 정보 확인용)
+    void Check_Item()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
+        if (Physics.Raycast(ray.origin, ray.direction * 20, out hitInfo, LayerMask))
+        {
+            if (hitInfo.transform != null && hitInfo.transform.tag == "Item")
+            {
+                ItemInfoAppear();
+            }
+            else
+                InfoDisappear();
+        }
+    }
+
+    void ItemInfoAppear()
+    {
+        PickUpActivate = true;
+        Item_Info.SetActive(true);
+        Item_Info.gameObject.transform.position = new Vector3(Camera.main.WorldToScreenPoint(hitInfo.transform.position).x,
+           Camera.main.WorldToScreenPoint(hitInfo.transform.position).y - 100f, Camera.main.WorldToScreenPoint(hitInfo.transform.position).z);
+        Item_Info_Text.text = "줍기(E): " + hitInfo.transform.GetComponent<ItemInfo>().item.itemName;
+
+    }
+
+    void InfoDisappear()
+    {
+        PickUpActivate = false;
+        Item_Info.gameObject.SetActive(false);
+    }
+}
