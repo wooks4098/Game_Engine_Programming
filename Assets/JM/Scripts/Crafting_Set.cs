644e2a3f4ab5d2d21b26d1bb2ee72a555f16bc55
camel96 <mrcamel7586@gmail.com>
2021-04-16 23:48:45 +0900

    크래프팅 UI 추가
    boundery type = int

---
+++ Assets/JM/Scripts/Crafting_Set.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[System.Serializable]
+public class Item_set
+{
+    public Item[] item;
+}
+
+public class Crafting_Set : MonoBehaviour
+{
+    public Item[] Self;
+    public Item[] Hut;
+    public Item[] CampFire;
+
+    public Item_set[] item_Set;
+    
+}
