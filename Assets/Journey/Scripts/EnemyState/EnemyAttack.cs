17776f1f81df08edc4626a6a455686f8ead79155
yuzi1125 <62372676+yuzi1125@users.noreply.github.com>
2021-04-20 13:16:11 +0900

    _

--- Assets/Journey/Scripts/EnemyState/EnemyAttack.cs
+++ Assets/Journey/Scripts/EnemyState/EnemyAttack.cs
@@ -1,45 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyAttack : IEnemyState
 {
     //이 상태는 늑대와 닭만 사용
 
-    private Enemy parent;
+    private Enemy enemy;
 
-    public void Enter(Enemy parent)
+    public void Enter(Enemy enemy)
     {
-        this.parent = parent;
+        this.enemy = enemy;
 
         //확인용
-        Debug.Log(parent.name + "의 Attack Enter");
+        Debug.Log(enemy.name + "의 Attack Enter");
     }
     public void Update()
     {
-        parent.nav.SetDestination(parent.target.position);
-        parent.transform.LookAt(parent.EnemyLookPoint);
+        enemy.nav.SetDestination(enemy.playerPos.position);
+        enemy.transform.LookAt(enemy.EnemyLookPoint);
 
         //공격 딜레이가 지났다면
-        if (Time.time >= parent.lastAttack + parent.delay)
+        if (Time.time >= enemy.lastAttack + enemy.delay)
         {
-            parent.player.OnDamage(parent.damage,parent.critical,parent.accuracy);
+            enemy.player.OnDamage(enemy.damage,enemy.critical,enemy.accuracy);
             Debug.Log("적이 플레이어를 공격");
             
-            parent.lastAttack = Time.time;
-            parent.ani.SetTrigger("Attack");
+            enemy.lastAttack = Time.time;
+            enemy.ani.SetTrigger("Attack");
         }
 
         //공격 범위에 없고, 추적 범위에 있을 때
-        if (!parent.isAttackArea() && parent.isFollowArea())
-            parent.ChangeState(new EnemyFollow());
+        if (!enemy.isAttackArea() && enemy.isFollowArea())
+            enemy.ChangeState(new EnemyFollow());
 
         //확인용
-        Debug.Log(parent.name + "의 Attack Update");
+        Debug.Log(enemy.name + "의 Attack Update");
     }
     public void Exit()
     {
         //확인용
-        Debug.Log(parent.name + "의 Attack Exit");
+        Debug.Log(enemy.name + "의 Attack Exit");
     }
 }
---
+++ Assets/Journey/Scripts/EnemyState/EnemyWalk.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyWalk : IEnemyState
+{
+    private Enemy parentEnemy;
+    private EnemySpawner enemySpawner;
+
+    public void Enter(Enemy enemy)
+    {
+        this.parentEnemy = enemy;
+        enemy.nav.ResetPath();
+
+        //확인용
+        Debug.Log(enemy.name + "의 Idle Enter");
+    }
+
+    public void Update()
+    {
+        parentEnemy.nav.SetDestination(parentEnemy.DestinationPos.position);
+
+        switch (parentEnemy.name)
+        {
+            case "Wolf":
+                //추적 범위에 있을 때
+                if (parentEnemy.isFollowArea())
+                    parentEnemy.ChangeState(new EnemyFollow());
+                break;
+            case "Chicken":
+                //플레이어에게 공격 당했고, 추적 범위에 있을 때 
+                if (parentEnemy.isDamaged && parentEnemy.isFollowArea())
+                    parentEnemy.ChangeState(new EnemyFollow());
+                break;
+            case "Sheep":
+                //플레이어에게 공격 당했고, 추적 범위에 있을 때
+                if (parentEnemy.isDamaged && parentEnemy.isFollowArea())
+                    parentEnemy.ChangeState(new EnemyRunaway());
+                break;
+        }
+
+        //확인용
+        Debug.Log(parentEnemy.name + "의 Idle Update");
+    }
+
+    public void Exit()
+    {
+        //확인용
+        Debug.Log(parentEnemy.name + "의 Idle Exit");
+    }
+
+
+}
